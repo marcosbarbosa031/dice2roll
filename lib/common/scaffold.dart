8770da6f1c7b1547d6e5669831d3c50a9d1582cd
Marcos Antonio Barbosa Filho <marcos.barbosa.filho@avanade.com>
2020-01-27 17:57:20 -0300

    Adding active variable to drawer

--- lib/common/scaffold.dart
+++ lib/common/scaffold.dart
@@ -2,16 +2,17 @@ import 'package:Dyce/common/drawer.dart';
 import 'package:flutter/material.dart';
 
 class AppScaffold extends StatelessWidget {
-  AppScaffold({Key key, this.title, this.body}): super(key: key);
+  AppScaffold({Key key, this.title, this.body, this.active}): super(key: key);
   final String title;
   final Widget body;
+  final int active;
 
   @override
   Widget build(BuildContext context) => Scaffold(
     appBar: AppBar(
       title: Text(title)
     ),
-    drawer: AppDrawer(),
+    drawer: AppDrawer(active: active),
     body: body
   );
 }
