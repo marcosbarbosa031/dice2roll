250f3f97645241c8cdaf183a181cfc2653516f97
Marcos Antonio Barbosa Filho <marcos.barbosa.filho@avanade.com>
2020-01-27 18:00:12 -0300

    Getting the data from sorage

--- lib/home/home.page.dart
+++ lib/home/home.page.dart
@@ -1,10 +1,10 @@
-import 'package:Dyce/authentication/authentication.dart';
-import 'package:Dyce/common/drawer.dart';
-import 'package:Dyce/common/theme.data.dart';
+import 'dart:convert';
+import 'dart:io';
+
 import 'package:Dyce/dice_presets/dice.presets.page.dart';
 import 'package:Dyce/dice_roll/dice.roll.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:path_provider/path_provider.dart';
 
 class HomePage extends StatefulWidget {
   HomePage({Key key, this.title}) : super(key: key);
@@ -13,8 +13,71 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  List<Map<String, dynamic>> presets = [];
+  Map<String, dynamic> defaultPreset = {
+    "name": "Default Preset",
+    "dices": [
+      {"qtd": 1, "type": 6, "result": "", "mod": 0}
+    ]
+  };
+
+  // @override
+  void initState () {
+    super.initState();
+    readcontent().then((content) {
+      print("content: $content");
+      if (content != false) {
+        List<Map<String, dynamic>> arr = new List<Map<String, dynamic>>();
+        arr.add(content);
+        setState(() {
+          presets = arr;
+        });
+      } else {
+        setState(() {
+          presets = null;
+        });
+      }
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Dicepresets();
+    if (presets != null) {
+      return Dicepresets(presets: presets);
+    } else {
+      return DiceRoll(preset: defaultPreset);
+    }
+  }
+
+  Future<String> get _localPath async {
+    final directory = await getApplicationDocumentsDirectory();
+    // For your reference print the AppDoc directory 
+    return directory.path;
+  }
+
+  Future<File> get _localFile async {
+    final path = await _localPath;
+    return File('$path/data.txt');
+  }
+
+  Future<File> writeContent() async {
+    final file = await _localFile;
+    // Write the file
+    return file.writeAsString('{"name":"Preset 1","dices":[{"qtd":1,"type":6,"result":"","mod":0},{"qtd":1,"type":20,"result":"","mod":0}]}');
+  }
+
+  readcontent() async {
+    try {
+      final file = await _localFile;
+      // Read the file
+      String stringData = await file.readAsString();
+      var content = json.decode(stringData);
+      // print(content['channelURL']);
+      return content;
+    } catch (e) {
+      // If there is an error reading, return a default String
+      print(e.toString());
+      return false;
+    }
   }
 }
